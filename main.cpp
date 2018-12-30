6cc5f4f818cefafdf6b5bab675abe7a8f5a419f3
Арляпов Савелий <sava_arl@mail.ru>
2018-12-30 21:57:59 +0800

    make simple doxygen

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
+/**
+ * @file main.cpp
+ * @author Sava
+ * @date 30 Dec 2018
+ * @brief File containing main code.
+ * This code is a solution for fourth
+ * home task.
+ */
 #include <iostream>
 #include <iterator>
 #include <string>
 #include <vector>
 #include <list>
 
+/// @private
 template <typename T>
 struct is_cont {
 	static const bool value = false;
 };
 
+/// @private
 template <typename T,typename Alloc>
 struct is_cont<std::vector<T, Alloc>> {
 	static const bool value = true;
 };
 
+/// @private
 template <typename T,typename Alloc>
 struct is_cont<std::list<T, Alloc>> {
 	static const bool value = true;
 };
 
+/// @private
 template <typename T, class = typename std::enable_if<is_cont<T>::value>::type>
 void print(const T& v) {
 	for(const auto &ip_part : v) {
@@ -29,6 +41,7 @@ void print(const T& v) {
 	std::cout << std::endl;
 }
 
+/// @private
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, T>::type print(const T &val) {
 	for (int i = sizeof(T); i > 0; --i) {
@@ -40,10 +53,12 @@ typename std::enable_if<std::is_integral<T>::value, T>::type print(const T &val)
 	return val;
 }
 
+/// @private
 void print(const std::string &val) {
 	std::cout << val << std::endl;
 }
 
+/// @private
 int main() {
 	try {
 		print(char(-1));
