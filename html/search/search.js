63b22f22b43769a571abc304aa5c8dc067c8391b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-30 18:36:41 +0000

    Deploy asvgit/hw_fourth to github.com/asvgit/hw_fourth.git:gh-pages

--- html/search/search.js
+++ html/search/search.js
@@ -7,14 +7,16 @@
 
 var indexSectionsWithContent =
 {
-  0: "m",
-  1: "m"
+  0: "im",
+  1: "m",
+  2: "i"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files"
+  1: "files",
+  2: "variables"
 };
 
 function convertToId(search)
